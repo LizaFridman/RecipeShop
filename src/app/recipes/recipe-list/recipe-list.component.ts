898774edb56a429e03e65f0535be4f3102d03ddf
LizaFridman <Liza.fridman39@gmail.com>
2019-04-22 14:42:49 +0300

    fixed compilation error

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,9 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
+// tslint:disable-next-line: max-line-length
     new Recipe('Dummy Recipe', 'This is an example recipe', 'https://prods3.imgix.net/images/articles/2017_04/Non-featured-Chorizo-egg-recipe.jpg?auto=format%2Ccompress&ixjsv=2.2.3&w=750'),
+// tslint:disable-next-line: max-line-length
     new Recipe('Dummy Recipe', 'This is an example recipe', 'https://prods3.imgix.net/images/articles/2017_04/Non-featured-Chorizo-egg-recipe.jpg?auto=format%2Ccompress&ixjsv=2.2.3&w=750')
   ];
 
