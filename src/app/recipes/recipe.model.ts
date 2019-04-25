502c3f469238232845fd2b84b8f494995a12c82a
LizaFridman <Liza.fridman39@gmail.com>
2019-04-25 16:01:02 +0300

    Creating Models for Recipe and shopping list
    53

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,4 +1,6 @@
-export class Recipe{
-  constructor(public name: string, public description: string, public imagePath: string){
+export class Recipe {
+
+  constructor(public name: string, public description: string, public imagePath: string) {
   }
+
 }
