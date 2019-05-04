ed67fb02edd1bd35528b0d4202ac46c8ea166753
LizaFridman <Liza.fridman39@gmail.com>
2019-05-04 16:57:39 +0300

    Added shopping list Add functionality

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,11 +7,12 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
 // tslint:disable-next-line: max-line-length
     new Recipe('Dummy Recipe', 'This is an example recipe', 'https://prods3.imgix.net/images/articles/2017_04/Non-featured-Chorizo-egg-recipe.jpg?auto=format%2Ccompress&ixjsv=2.2.3&w=750'),
 // tslint:disable-next-line: max-line-length
-    new Recipe('Dummy Recipe', 'This is an example recipe', 'https://prods3.imgix.net/images/articles/2017_04/Non-featured-Chorizo-egg-recipe.jpg?auto=format%2Ccompress&ixjsv=2.2.3&w=750')
+    new Recipe('Second Dummy Recipe', 'This is a second example recipe', 'https://i.ytimg.com/vi/XFW74wMJ7Ok/hqdefault.jpg')
   ];
 
   constructor() { }
@@ -19,4 +20,7 @@ export class RecipeListComponent implements OnInit {
   ngOnInit() {
   }
 
+  onRecipeSelected(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe);
+  }
 }
